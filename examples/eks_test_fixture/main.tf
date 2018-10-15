fe3f406ee3922b38ed9fe8a93b5365f8f4b86d60
Ferhat Yildiz <ferhat.yildiz@turingts.com>
2018-10-15 13:54:50 +0200

    added lookups and moved maps and vars to terraform.tfvars

--- examples/eks_test_fixture/main.tf
+++ examples/eks_test_fixture/main.tf
@@ -5,53 +5,28 @@ terraform {
 provider "aws" {
   version = ">= 1.24.0"
   region  = "${var.region}"
-}
-
-provider "random" {
-  version = "= 1.3.1"
+  profile = "${var.profile}"
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "test-eks-${random_string.suffix.result}"
-
-  # the commented out worker group list below shows an example of how to define
-  # multiple worker groups of differing configurations
-  # worker_groups = [
-  #   {
-  #     asg_desired_capacity = 2
-  #     asg_max_size = 10
-  #     asg_min_size = 2
-  #     instance_type = "m4.xlarge"
-  #     name = "worker_group_a"
-  #     additional_userdata = "echo foo bar"
-  #     subnets = "${join(",", module.vpc.private_subnets)}"
-  #   },
-  #   {
-  #     asg_desired_capacity = 1
-  #     asg_max_size = 5
-  #     asg_min_size = 1
-  #     instance_type = "m4.2xlarge"
-  #     name = "worker_group_b"
-  #     additional_userdata = "echo foo bar"
-  #     subnets = "${join(",", module.vpc.private_subnets)}"
-  #   },
-  # ]
+  cluster_name = "${var.cluster_name}"
 
   worker_groups = [
     {
       instance_type       = "t2.small"
       additional_userdata = "echo foo bar"
-      subnets             = "${join(",", module.vpc.private_subnets)}"
+      subnets             = "${join(",", data.aws_subnet_ids.private.ids)}"
     },
     {
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
-      subnets                       = "${join(",", module.vpc.private_subnets)}"
+      subnets                       = "${join(",", data.aws_subnet_ids.private.ids)}"
       additional_security_group_ids = "${aws_security_group.worker_group_mgmt_one.id},${aws_security_group.worker_group_mgmt_two.id}"
     },
   ]
+
   tags = {
     Environment = "test"
     GithubRepo  = "terraform-aws-eks"
@@ -60,15 +35,10 @@ locals {
   }
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   description = "SG to be applied to all *nix machines"
-  vpc_id      = "${module.vpc.vpc_id}"
+  vpc_id      = "${data.aws_vpc.main.id}"
 
   ingress {
     from_port = 22
@@ -83,7 +53,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
 
 resource "aws_security_group" "worker_group_mgmt_two" {
   name_prefix = "worker_group_mgmt_two"
-  vpc_id      = "${module.vpc.vpc_id}"
+  vpc_id      = "${data.aws_vpc.main.id}"
 
   ingress {
     from_port = 22
@@ -98,7 +68,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
-  vpc_id      = "${module.vpc.vpc_id}"
+  vpc_id      = "${data.aws_vpc.main.id}"
 
   ingress {
     from_port = 22
@@ -113,25 +83,12 @@ resource "aws_security_group" "all_worker_mgmt" {
   }
 }
 
-module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "1.14.0"
-  name               = "test-vpc"
-  cidr               = "10.0.0.0/16"
-  azs                = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  tags               = "${merge(local.tags, map("kubernetes.io/cluster/${local.cluster_name}", "shared"))}"
-}
-
 module "eks" {
   source                               = "../.."
   cluster_name                         = "${local.cluster_name}"
-  subnets                              = ["${module.vpc.private_subnets}"]
+  subnets                              = "${data.aws_subnet_ids.private.ids}"
   tags                                 = "${local.tags}"
-  vpc_id                               = "${module.vpc.vpc_id}"
+  vpc_id                               = "${data.aws_vpc.main.id}"
   worker_groups                        = "${local.worker_groups}"
   worker_group_count                   = "2"
   worker_additional_security_group_ids = ["${aws_security_group.all_worker_mgmt.id}"]
--- examples/eks_test_fixture/variables.tf
+++ examples/eks_test_fixture/variables.tf
@@ -1,44 +1,49 @@
 variable "region" {
-  default = "us-west-2"
+  description = "AWS Region"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+}
+
+variable "profile" {
+  description = "Which profile to use"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+}
+
+variable "vpc_tag" {
+  description = "Tag to use in order to filter subnets"
+}
+
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.vpc_name}"]
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = "${data.aws_vpc.main.id}"
+
+  tags {
+    Name = "${var.vpc_tag}"
+  }
 }
 
 variable "map_accounts" {
   description = "Additional AWS account numbers to add to the aws-auth configmap."
   type        = "list"
-
-  default = [
-    "777777777777",
-    "888888888888",
-  ]
 }
 
 variable "map_roles" {
   description = "Additional IAM roles to add to the aws-auth configmap."
   type        = "list"
-
-  default = [
-    {
-      role_arn = "arn:aws:iam::66666666666:role/role1"
-      username = "role1"
-      group    = "system:masters"
-    },
-  ]
 }
 
 variable "map_users" {
   description = "Additional IAM users to add to the aws-auth configmap."
   type        = "list"
-
-  default = [
-    {
-      user_arn = "arn:aws:iam::66666666666:user/user1"
-      username = "user1"
-      group    = "system:masters"
-    },
-    {
-      user_arn = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
-      group    = "system:masters"
-    },
-  ]
 }
